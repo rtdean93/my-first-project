ee623c1ff343b352f6547a8eecf5affd379c5719
rtdean93 <bobby@deanhome.net>
2026-08-20 21:53:12 -0500

    Initial commit for new project

---
+++ onStart.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This file contains bash commands that will be executed when the Project container starts.
+#
+# Note: Passwordless sudo is available during the execution of this script
+# Note: This file may be removed if you don't need to use it
